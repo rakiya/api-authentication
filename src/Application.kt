896db78a311a03f322590769b4c879d62a001642
Ryutaro Akiya <>
2020-12-02 13:34:24 +0900

    NginxをプロキシとしたCORSに対応

--- src/Application.kt
+++ src/Application.kt
@@ -14,26 +14,19 @@ import habanero.models.CertificationToken
 import habanero.models.RefreshToken
 import habanero.routes.*
 import habanero.utils.RSA
-import io.ktor.application.Application
-import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.auth.Authentication
-import io.ktor.auth.jwt.jwt
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.features.*
-import io.ktor.gson.gson
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
-import io.ktor.http.HttpStatusCode
-import io.ktor.locations.KtorExperimentalLocationsAPI
-import io.ktor.locations.Locations
-import io.ktor.request.path
-import io.ktor.response.respond
-import io.ktor.response.respondText
+import io.ktor.gson.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.request.*
+import io.ktor.response.*
 import io.ktor.routing.get
 import io.ktor.routing.route
 import io.ktor.routing.routing
-import io.ktor.util.KtorExperimentalAPI
+import io.ktor.util.*
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.koin.ktor.ext.Koin
@@ -42,7 +35,6 @@ import org.litote.kmongo.reactivestreams.KMongo
 import org.slf4j.event.Level
 import java.text.DateFormat
 import kotlin.time.ExperimentalTime
-import kotlin.time.days
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -62,9 +54,9 @@ fun Application.injectDependency() {
             single {
                 environment.config.run {
                     MailClient(
-                        getString("mail.host"), getString("mail.port"),
-                        getString("mail.username"), getString("mail.password"),
-                        getString("mail.address.no-reply")
+                            getString("mail.host"), getString("mail.port"),
+                            getString("mail.username"), getString("mail.password"),
+                            getString("mail.address.no-reply")
                     )
                 }
             }
@@ -90,6 +82,7 @@ fun Application.injectDependency() {
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
     install(Locations) { }
 
     install(CORS) {
@@ -99,19 +92,22 @@ fun Application.module(testing: Boolean = false) {
         method(HttpMethod.Put)
         method(HttpMethod.Delete)
         method(HttpMethod.Patch)
-        header(HttpHeaders.ContentType)
-        header(HttpHeaders.Authorization)
+        header(HttpHeaders.Host)
+        header(HttpHeaders.Origin)
         header(HttpHeaders.Accept)
+        header(HttpHeaders.AccessControlRequestHeaders)
+        header(HttpHeaders.AccessControlRequestMethod)
+        header(HttpHeaders.Connection)
+        header(HttpHeaders.Authorization)
+        header(HttpHeaders.ContentType)
         header(HttpHeaders.Referrer)
         header(HttpHeaders.UserAgent)
-        header("Sec-Fetch-Dest")
-        host("localhost:8081", schemes = listOf("http", "https"))
+        anyHost()
         allowCredentials = true
     }
 
     install(CallLogging) {
-//        level = if (testing) Level.TRACE else Level.WARN
-        level = Level.WARN
+        level = Level.DEBUG
         filter { call -> call.request.path().startsWith("/") }
     }
 
